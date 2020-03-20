ffaf2cb20c2ad04119ef10d32a551caeed65d8b7
Gustavo Bordoni <bordoni.dev@gmail.com>
2020-03-20 17:20:47 -0400

    Updated

--- src/Tribe/Assets.php
+++ src/Tribe/Assets.php
@@ -29,7 +29,7 @@ class Assets extends \tad_DI52_ServiceProvider {
 	 *
 	 * @var string
 	 */
-	public static $group_key = 'happening-now';
+	public static $group_key = 'events-control';
 
 	/**
 	 * Binds and sets up implementations.
@@ -37,38 +37,6 @@ class Assets extends \tad_DI52_ServiceProvider {
 	 * @since 1.0.0
 	 */
 	public function register() {
-		$plugin = tribe( Main::class );
 
-		tribe_asset(
-			$plugin,
-			'tribe-ext-events-happening-now',
-			'style.css',
-			[
-				'tribe-common-full-style',
-				'tribe-events-views-v2-skeleton',
-			],
-			null,
-			[
-				'groups' => [ static::$group_key ],
-			]
-		);
-
-		$overrides_stylesheet = Tribe__Events__Templates::locate_stylesheet( 'tribe-events/tribe-ext-events-happening-now.css' );
-
-		if ( ! empty( $overrides_stylesheet ) ) {
-			tribe_asset(
-				$plugin,
-				'tribe-ext-events-happening-now-override',
-				$overrides_stylesheet,
-				[
-					'tribe-common-full-style',
-					'tribe-events-views-v2-skeleton',
-				],
-				null,
-				[
-					'groups' => [ static::$group_key ],
-				]
-			);
-		}
 	}
 }
